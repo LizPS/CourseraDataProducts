a77dd886e01265a87f2b7d5c5297bcc826920d1a
Liz Supinski <liz@supinski.net>
2014-09-17 16:14:52 -0400

    working version but needs some cleanup

---
+++ server.R
@@ -0,0 +1,38 @@
+# stuff that runs once when app is launched
+library(shiny)
+library(ggplot2)
+library(reshape2)
+source("thy.R")
+instructTxt <- "<p>This is where the  <em>instructions</em> go.</p>"
+
+
+shinyServer(function(input, output, session){
+#everything in here run once each time a user visits the app
+        myvalues <- reactiveValues()
+        myvalues$df <-data.frame(Date=as.character("2012-01-01"), TSH=2.95, 
+                                 FreeT4=1.25, FreeT3= 3.2,
+                                 stringsAsFactors=FALSE)
+        
+        newEntry <- observe({
+                if(input$update > 0) {
+                        newLine <- isolate(list(as.character.Date(input$Date),
+                                                as.numeric(input$TSH),
+                                                as.numeric(input$T4),
+                                                as.numeric(input$T3)))
+                        myvalues$df <- isolate(rbind(myvalues$df, newLine))
+                        #isolate(myvalues$df[nrow(myvalues$df) + 1, ] <- newLine)
+                }
+        })
+
+        
+        output$testing <- renderText({
+                        class(myvalues$df)
+                })
+        output$instruct <- renderText({instructTxt})
+        output$graph <- renderPlot ({
+        #everything in here changes each time a user changes a widget that this output relies on
+                newdf <- data.frame(myvalues$df)
+                t_graph(myvalues$df, input$tx, input$TxDate)
+        })
+        output$txtable <- renderTable({myvalues$df})
+})
---
+++ ui.R
@@ -0,0 +1,33 @@
+library(shiny)
+
+shinyUI(fluidPage(
+	titlePanel("How am I doing?"),
+	fluidRow(
+                column(3, wellPanel(
+        		dateInput(inputId = "Date",
+        			label = "Test Date",
+                                startview="year"),    
+        		textInput(inputId = "TSH",
+        			label = "TSH test value",
+        			value = ""),
+        		textInput(inputId = "T4",
+        			label = "Free T4 test value",
+        			value = ""),
+        		textInput(inputId = "T3",
+        			label = "Free T3 test value",
+        			value = ""),
+        		checkboxInput(inputId = "tx",
+        			label = "Include date you began treatment on graph?"),
+        		conditionalPanel(
+        			condition = "input.tx == true",
+        			dateInput(inputId ="TxDate", label = "Date Treatment Began")),
+                        actionButton("update", "Update Table")
+	)),
+	column(6,
+		tabsetPanel(type = "tabs",
+                        tabPanel("Instructions", htmlOutput("instruct")),
+			tabPanel("Your Results", plotOutput("graph")), #verbatimTextOutput("testing")),
+			tabPanel("Your Data", tableOutput("txtable"))
+		)
+	)
+)))
